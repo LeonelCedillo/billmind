0b335aae433e906dfcd5c2aec6dba23948a9cec2
LeonelCedillo <leonel.cs.979@gmail.com>
2026-06-10 14:56:10 -0700

    feat: add refresh and revoke refresh token routes

--- src/server/routes/authRoutes.ts
+++ src/server/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { handlerLogin } from "../api/login.js";
 import { handlerRegister } from "../api/users.js";
+import { handlerRefresh, handlerRevoke } from "../api/refresh.js";
 
 
 const router = Router();
@@ -9,7 +10,7 @@ const router = Router();
 router.post("/register", async (req, res, next) => {
   try {
     await handlerRegister(req, res);
-  }catch (err) {
+  } catch (err) {
     next(err); // Pass the error to the errorMiddleware
   }
 });
@@ -18,5 +19,12 @@ router.post("/login", async (req, res, next) => {
   Promise.resolve(handlerLogin(req, res)).catch(next);
 });
 
+router.post("/refresh", async (req, res, next) => {
+  Promise.resolve(handlerRefresh(req, res)).catch(next);
+});
+
+router.post("/revoke", async (req, res, next) => {
+  Promise.resolve(handlerRevoke(req, res)).catch(next);
+});
 
 export default router;
